c3e0f58c05ac3d32160103823a6604ddb5684968
goltibix <gerardoroyal79.com>
2024-04-16 15:36:34 -0700

    change on SearchBar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
-
+import SearchBar from './components/SearchBar'
+//import ImageList from '.components/ImageList'
+//import ImageShow from '.components/ImageShow'
 import './App.css'
 
 function App() {
+  const [images, setImages] = useState([])
+
+  const handleSubmit = async(term) => {
+    console.log('usted esta buscando con: ', term)
+  }
   
   return (
       <div>
-       <h1> hello there</h1>
+       <h1>APP</h1>
+       <SearchBar onSubmit={handleSubmit} />
       </div>
   )
 }
